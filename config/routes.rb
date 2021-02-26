22eec9978b34af7c56a4aba107ad1001c73a8d8d
Gustavo Carvalho <gsc13@tuta.io>
2021-02-26 16:31:11 -0300

    Revert "Fix rubocop offences"
    This reverts commit ef8d4b3d1b1b817b1253233b6ef11ae6370e0da1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  root 'posts#index'
+	root 'posts#index'
 
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :posts, only: %i[new create index]
+  resources :posts, only: [:new, :create, :index]
 end
