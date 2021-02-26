d568f20eac7345b6548a7ecfe2b3dd513b9d580a
Gustavo Carvalho <gsc13@tuta.io>
2021-02-26 15:10:45 -0300

    Add sign up params to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'posts#index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :posts, only: [:new, :create, :index]
 end
